e65a3ac28f89d2d2f0a3eec907ddb383d644fc6f
Pat Downey <pat.downey@gmail.com>
2015-04-08 10:27:36 +0100

    Add time test and travis.yml

---
+++ time_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDefaultTimeToUnixNanoAndBackAgain(t *testing.T) {
+	var defaultTime time.Time // 0001-01-01 00:00:00 +0000 UTC
+
+	encodedDefaultTime := defaultTime.UnixNano() //gives -6795364578871345152
+
+	decodedDefaultTime := time.Unix(0, encodedDefaultTime)
+
+	if defaultTime != decodedDefaultTime {
+		t.Errorf("expected: %v, got %v", defaultTime, decodedDefaultTime)
+	}
+}
+
+func TestDefaultTimeToUnixAndBackAgain(t *testing.T) {
+	var defaultTime time.Time // 0001-01-01 00:00:00 +0000 UTC
+
+	encodedDefaultTime := defaultTime.Unix() //gives -62135596800
+
+	decodedDefaultTime := time.Unix(encodedDefaultTime, 0)
+
+	if defaultTime != decodedDefaultTime {
+		t.Errorf("expected: %v, got %v", defaultTime, decodedDefaultTime)
+	}
+}
